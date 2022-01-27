ae1abc427414e469be285b712a94d3e4d831a20b
Scott Bailey <csb5t@virginia.edu>
2022-01-27 14:17:35 -0500

    force annotation drawing

--- components/Mirador.js
+++ components/Mirador.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import mirador from "mirador";
 import annotationPlugins from "mirador-annotations/es/index";
 import LocalStorageAdapter from "mirador-annotations/es/LocalStorageAdapter";
+import AnnototAdapter from "mirador-annotations/es/AnnototAdapter";
 
 export default function Mirador(props) {
   const config = {
@@ -9,17 +10,20 @@ export default function Mirador(props) {
     annotation: {
       adapter: (canvasId) =>
         new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
-      exportLocalStorageAnnotations: true, // display annotation JSON export button
+      // adapter: (canvasId) => new AnnototAdapter(canvasId, "localhost:3000"),
+      exportLocalStorageAnnotations: true, // display annotation JSON export button,
     },
     window: {
       defaultSideBarPanel: "annotations",
       sideBarOpenByDefault: true,
+      highlightAllAnnotations: true,
+      forceDrawAnnotations: true,
     },
     catalog: [
-      {
-        manifestId:
-          "https://sad-leakey-4368a8.netlify.app/img/derivatives/iiif/dc/manifest.json",
-      },
+      // {
+      //   manifestId:
+      //     "https://sad-leakey-4368a8.netlify.app/img/derivatives/iiif/dc/manifest.json",
+      // },
     ],
     windows: [
       {
